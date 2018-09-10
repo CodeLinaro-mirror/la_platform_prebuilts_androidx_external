a53ba3093ea0220f7100066539ec3fd649be5761
Jeff Gaston <jeffrygaston@google.com>
2018-09-10 14:15:16 -0400

    Import metalava 1.1.2-SNAPSHOT from build 5018572
    Bug: 87457009
    
    Change-Id: I9e0874acd303d05fd111614d2f0a47dc847fad88

---
+++ com/android/metalava/update_metalava.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+function usage() {
+  echo "Updates metalava*.jar"
+  echo "Usage: update_metalava.sh <build-id>"
+  exit 1
+}
+
+
+buildId="$1"
+if [ "$buildId" == "" ]; then
+  usage
+fi
+
+tempDir=/tmp
+destDir="$(cd $(dirname $0) && pwd)"
+
+function downloadArtifact() {
+  cd "$tempDir"
+  echo "Downloading metalava from build $buildId"
+  /google/data/ro/projects/android/fetch_artifact --bid "$buildId" --target sdk_phone_armv7-sdk 'metalava.jar'
+}
+downloadArtifact
+
+function getVersionNumber() {
+  versionNumber="$(java -jar metalava.jar --version 2>/dev/null || echo 'unknown')"
+  if echo "$versionNumber" | grep "unknown" >/dev/null; then
+    versionNumber="1.1.2-SNAPSHOT"
+    echo "Could not parse version number of metalava.jar; assuming version $versionNumber"
+  fi
+}
+getVersionNumber
+
+function removeOldMetalava() {
+  find "$destDir" -mindepth 1 -maxdepth 1 -type d | xargs --no-run-if-empty rm -rf
+  #find "$destDir" -mindepth 1 -maxdepth 1 -type d | xargs echo removing
+}
+removeOldMetalava
+
+function copyNewMetalava() {
+  subdir="${destDir}/${versionNumber}"
+  mkdir -p "$subdir"
+  cp metalava.jar "${subdir}/metalava-${versionNumber}-shadow.jar"
+}
+copyNewMetalava
+
+function gitCommit() {
+  cd "${destDir}"
+  git add .
+  git commit -m "Import metalava ${versionNumber} from build ${buildId}"
+}
+gitCommit
